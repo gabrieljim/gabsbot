f434a253c25f564ba20d4b44136c4877f9ab9782
gabrieljim <gabrieljim641@gmail.com>
2020-04-26 09:34:21 -0400

    seems to work

--- functions-build/index.js
+++ functions-build/index.js
@@ -2,12 +2,17 @@ const Telegraf = require("telegraf");
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const start = require("./bot/start");
 
+const reply = ctx => {
+	console.log(ctx);
+	ctx.reply("holis");
+}
+
 bot.start(ctx => {
 	return start(ctx);
 });
 
 bot.on("text", ctx => {
-	return ctx.reply("holis");
+	return reply(ctx);
 });
 
 bot.on("inline_query", ctx => {
