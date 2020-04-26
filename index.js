ae7428becc8f31f02cb6cc4df37d29bd0ad4128c
gabrieljim <gabrieljim641@gmail.com>
2020-04-26 10:28:33 -0400

    hacked this shit

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config();
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
-console.log(process.env.BOT_TOKEN);
 
 bot.on("polling_error", err => console.log(err));
 
