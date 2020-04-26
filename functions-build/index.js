28816b78cbbd09cd6da7ba70d45c2df4ac400f4e
gabrieljim <gabrieljim641@gmail.com>
2020-04-26 09:31:38 -0400

    s

--- functions-build/index.js
+++ functions-build/index.js
@@ -3,12 +3,12 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 const start = require("./bot/start");
 
 bot.start(ctx => {
-	return start(ctx)
+	return start(ctx);
 });
 
-bot.message(ctx => {
-	console.log(ctx)
-})
+bot.on("text", ctx => {
+	return ctx.reply("holis");
+});
 
 bot.on("inline_query", ctx => {
 	return console.log(ctx);
