912e28196285190de89096412589311e92ae05af
gabrieljim <gabrieljim641@gmail.com>
2020-04-26 10:49:57 -0400

    wtf is happening

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 require("dotenv").config();
 process.env.NTBA_FIX_319 = 1;
 const TelegramBot = require("node-telegram-bot-api");
-const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 bot.on("polling_error", err => console.log(err));
 
 bot.on("message", msg => {
-	bot.sendMessage(msg.chat.id, "hola");
+	bot.sendMessage(msg.chat.id, "holis");
 });
-
-/*
-const reply = ctx => {
-	console.log(ctx);
-	ctx.reply("holis");
-}
-
-bot.start(ctx => {
-	return start(ctx);
-});
-
-bot.on("text", ctx => {
-	return reply(ctx);
-});
-
-bot.on("inline_query", ctx => {
-	return console.log(ctx);
-});
-
-exports.handler = async event => {
-	await bot.handleUpdate(JSON.parse(event.body));
-	return { statusCode: 200, body: "" };
-};
-*/
